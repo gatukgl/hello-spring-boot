20c9acab669f5a05fe6f2033bd9e9e36d6329cb2
Gatuk Chattanon <gatukgl@gmail.com>
2020-06-28 13:40:52 +0700

    Separate business logic to service and support update employee

--- src/main/java/com/example/springbootonline2/controller/EmployeeController.java
+++ src/main/java/com/example/springbootonline2/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.springbootonline2.controller;
 
 import com.example.springbootonline2.domain.Employee;
 import com.example.springbootonline2.repository.EmployeeRepository;
+import com.example.springbootonline2.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,29 +12,29 @@ import java.util.List;
 @RequestMapping(value = "/employees")
 public class EmployeeController {
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private EmployeeService employeeService;
 
     // @RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public List<Employee> listAllEmployee() {
-        return employeeRepository.listAll();
+        return employeeService.listAll();
     }
 
     // @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @GetMapping("/{id}")
     public Employee getEmployee(@PathVariable Integer id) {
-       return employeeRepository.findById(id);
+       return employeeService.findById(id);
     }
 
     // @RequestMapping(method = RequestMethod.POST)
     @PostMapping
     public void  createEmployee(@RequestBody Employee employee) {
-       employeeRepository.save(employee);
+       employeeService.save(employee);
     }
 
     // @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     @PutMapping("/{id}")
-    public void updateEmployee(@PathVariable Integer Id, @RequestBody Employee employee) {
-
+    public void updateEmployee(@PathVariable Integer id, @RequestBody Employee employee) {
+        employeeService.update(id, employee);
     }
 }
---
+++ src/main/java/com/example/springbootonline2/service/EmployeeService.java
@@ -0,0 +1,40 @@
+package com.example.springbootonline2.service;
+
+import com.example.springbootonline2.domain.Employee;
+import com.example.springbootonline2.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> listAll() {
+        return employeeRepository.listAll();
+    }
+
+    public Employee findById(Integer id) {
+        return employeeRepository.findById(id);
+    }
+
+    @Transactional
+    public void save(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Transactional
+    public void update(Integer id, Employee employee) {
+        Employee employeeEntity = findById(id);
+        if (employee == null) {
+            throw new RuntimeException("Employee not found!");
+        }
+
+        employeeEntity.setFirstName(employee.getFirstName());
+        employeeEntity.setLastName(employee.getLastName());
+        employeeRepository.save(employeeEntity);
+    }
+}
