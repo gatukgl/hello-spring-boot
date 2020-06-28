a5bfc00937d8beafc2a718677421be19e60d25f9
Gatuk Chattanon <gatukgl@gmail.com>
2020-06-28 15:05:59 +0700

    Remove unused comment

--- src/main/java/com/example/springbootonline2/controller/EmployeeController.java
+++ src/main/java/com/example/springbootonline2/controller/EmployeeController.java
@@ -13,25 +13,21 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    // @RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public List<Employee> listAllEmployee() {
         return employeeService.listAll();
     }
 
-    // @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @GetMapping("/{id}")
     public Employee getEmployee(@PathVariable Integer id) {
        return employeeService.findById(id);
     }
 
-    // @RequestMapping(method = RequestMethod.POST)
     @PostMapping
     public void  createEmployee(@RequestBody Employee employee) {
        employeeService.save(employee);
     }
 
-    // @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     @PutMapping("/{id}")
     public void updateEmployee(@PathVariable Integer id, @RequestBody Employee employee) {
         employeeService.update(id, employee);
