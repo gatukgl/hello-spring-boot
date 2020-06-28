cb48c29b2426a1b4dd0f18c67861abc4604bb150
Gatuk Chattanon <gatukgl@gmail.com>
2020-06-28 10:02:36 +0700

    Add put method to controller

--- src/main/java/com/example/springbootonline2/controller/HelloController.java
+++ src/main/java/com/example/springbootonline2/controller/HelloController.java
@@ -37,4 +37,10 @@ public class HelloController {
     public void postMessage(@RequestBody MessageInfo messageInfo) {
         System.out.println("Message: " + messageInfo.getMessage());
     }
+
+    @RequestMapping(value = "/hello/{id}", method = RequestMethod.PUT)
+    public void putMessage(@PathVariable String id, @RequestBody MessageInfo messageInfo) {
+        System.out.println("id: " + id);
+        System.out.println("messageInfo: " + messageInfo.getMessage());
+    }
 }
